d820d06a14a82f9a7cf39025f3dc3f07e6de4175
Khoa <nguyenvietkhoa12031993@gmail.com>
2020-12-15 14:26:11 +0700

    code commit

---
+++ assets/js/main.js
@@ -0,0 +1,129 @@
+import handleSelector from './selector.js';
+import sortTable from './sortTable.js';
+
+const URL_API = 'https://coronavirus-19-api.herokuapp.com/';
+const tbody = document.querySelector('tbody');
+const globalCard = document.querySelector('.global-block');
+const countriesSelector = document.querySelector('.countries-selector');
+const countriesSelectorBtn = document.querySelector('.countries-selector-btn');
+
+function formatNumber(number) {
+  if (typeof number === 'number') {
+    return number.toLocaleString('us-US');
+  }
+  return '...';
+}
+
+/* Handle data of global */
+fetch(`${URL_API}all`)
+  .then(res => res.json())
+  .then(result => showResultsGlobal(result));
+
+function showResultsGlobal(result) {
+  let htmls = '';
+  htmls +=
+    `
+  <div class="global-card">
+  <span>Global</span>
+  <h3>Coronavirus cases</h3>
+  <span>${formatNumber(result.cases)}</span>
+  </div>
+  <div class="global-card">
+  <span>Global</span>
+  <h3>Deaths</h3>
+  <span>${formatNumber(result.recovered)}</span>
+  </div>
+  <div class="global-card">
+  <span>Global</span>
+  <h3>Recovered</h3>
+  <span>${formatNumber(result.deaths)}</span>
+  </div>
+  `
+  globalCard.innerHTML += htmls;
+}
+
+
+/* Handle data of countries */
+fetch(`${URL_API}countries`)
+  .then(res => res.json())
+  .then(result => showResultsContries(result));
+
+function showResultsContries(results) {
+  let htmls = '';
+  let html = '';
+  const countriesArray = [];
+
+  results.forEach(result => {
+    countriesArray.push(result.country)
+    
+    htmls += `
+    <tr>
+    <td></td>
+    <td>${result.country}</td>
+    <td>${formatNumber(result.cases)}</td>
+    <td>${formatNumber(result.todayCases)}</td>
+    <td>${formatNumber(result.deaths)}</td>
+    <td>${formatNumber(result.todayDeaths)}</td>
+    <td>${formatNumber(result.recovered)}</td>
+    <td>${formatNumber(result.active)}</td>
+    </tr>
+    `;
+  })
+
+  countriesArray.sort().forEach(country => {
+    html += `<li class="country">${country}</li>`;
+  });
+
+  tbody.innerHTML += htmls;
+  countriesSelector.innerHTML += html;
+
+}
+
+/* Handle data of selector */
+countriesSelector.addEventListener('click', (e) => {
+  let countryName = '';
+  if (e.target.className === 'country') {
+    e.target.textContent !== 'All Country' ? countryName = e.target.textContent : countryName;
+    countriesSelectorBtn.innerHTML = `${e.target.textContent} <span>&#9660;</span>`;
+  }
+
+  fetch(`${URL_API}countries/${countryName}`)
+    .then(res => res.json())
+    .then(result => showResultsSelector(result));
+})
+
+function showResultsSelector(results) {
+  let htmls = '';
+  if (Array.isArray(results)) {
+    results.forEach(result => {
+      htmls += `
+      <tr>
+      <td></td>
+      <td>${result.country}</td>
+      <td>${formatNumber(result.cases)}</td>
+      <td>${formatNumber(result.todayCases)}</td>
+      <td>${formatNumber(result.deaths)}</td>
+      <td>${formatNumber(result.todayDeaths)}</td>
+      <td>${formatNumber(result.recovered)}</td>
+      <td>${formatNumber(result.active)}</td>
+      </tr>
+      `;
+    })
+  } else {
+    htmls += `
+    <tr>
+    <td></td>
+    <td>${results.country}</td>
+    <td>${formatNumber(results.cases)}</td>
+    <td>${formatNumber(results.todayCases)}</td>
+    <td>${formatNumber(results.deaths)}</td>
+    <td>${formatNumber(results.todayDeaths)}</td>
+    <td>${formatNumber(results.recovered)}</td>
+    <td>${formatNumber(results.active)}</td>
+    </tr>
+    `;
+  }
+  tbody.innerHTML = '';
+  tbody.innerHTML += htmls;
+}
+
---
+++ assets/js/sortTable.js
@@ -0,0 +1,46 @@
+function sortTable(table, column, asc) {
+  const dirModifier = asc ? 1 : -1;
+  const tbody = table.tBodies[0];
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  /* Sort each row */
+  const sortedRows = rows.sort((a, b) => {
+    const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+    const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+    const aColTextToNumber = Number(aColText.replace(/\,|\.|\s/g, ''));
+    const bColTextToNumber = Number(bColText.replace(/\,|\.|\s/g, ''));
+    const isNumber = /^\d+$/;
+    if (isNumber.test(aColTextToNumber) && isNumber.test(bColTextToNumber)) {
+      return aColTextToNumber > bColTextToNumber ? (1 * dirModifier) : (-1 * dirModifier);
+    } else {
+      return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    }
+  });
+
+  /* Remove all existing tr from table */
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  /* ReAdd newSorted rows */
+  tbody.append(...sortedRows);
+
+  /* Remember colunm is currently sorted */
+  table.querySelectorAll('th').forEach(th => th.classList.remove('th-sort-asc', 'th-sort-desc'));
+  table.querySelector(`th:nth-child(${column + 1})`).classList.toggle('th-sort-asc', asc);
+  table.querySelector(`th:nth-child(${column + 1})`).classList.toggle('th-sort-desc', !asc);
+  console.log(asc);
+}
+
+document.querySelectorAll('table th:not(:first-child)').forEach(headerCell => {
+  headerCell.addEventListener("click", () => {
+    const tableElement = headerCell.parentElement.parentElement.parentElement;
+    const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+    const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+    sortTable(tableElement, headerIndex, !currentIsAscending);
+  });
+});
+
+export default sortTable;
+
